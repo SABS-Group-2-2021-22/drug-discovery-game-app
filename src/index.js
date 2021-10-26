81b218b98e3326fa65b6e66bb0561522a8aeee73
Nele Quast <nele.quast@dtc.ox.ac.uk>
2021-10-26 15:19:09 +0100

    initial commit w./ template mol imgs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import rgroup_img1 from './A01.png'
+import rgroup_img2 from './A02.png'
+import rgroup_img3 from './A03.png'
+import rgroup_img4 from './A04.png'
+import rgroup_img5 from './A05.png'
+import rgroup_img6 from './A06.png'
+  class RGroupWidget extends React.Component{
+    constructor(props){
+      super(props);
+      this.state = {
+        img: props.img,
+      };
+    }
+    render(){
+      return (
+        <img src={this.state.img} alt='R Group'/>
+      )
+    }
+  }
+  
+  class App extends React.Component {
+    render() {
+      return (
+        <div className="app">
+          <div className="app-rgroup-row1">
+            <RGroupWidget img={rgroup_img1}/>
+            <RGroupWidget img={rgroup_img2}/>
+          </div>
+          <div className="app-rgroup-row2">
+            <RGroupWidget img={rgroup_img3}/>
+            <RGroupWidget img={rgroup_img4}/>
+          </div>
+          <div className="app-rgroup-row3">
+            <RGroupWidget img={rgroup_img5}/>
+            <RGroupWidget img={rgroup_img6}/>
+          </div>
+        </div>
+      );
+    }
+  }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // ========================================
+  
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
+  
