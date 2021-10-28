42d72d113167acd2b635e218fb6285f7542c347e
Nele Quast <nele.quast@dtc.ox.ac.uk>
2021-10-28 12:00:39 +0100

    molecule selection implemented

--- src/index.js
+++ src/index.js
@@ -1,46 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import rgroup_img1 from './A01.png'
-import rgroup_img2 from './A02.png'
-import rgroup_img3 from './A03.png'
-import rgroup_img4 from './A04.png'
-import rgroup_img5 from './A05.png'
-import rgroup_img6 from './A06.png'
   class RGroupWidget extends React.Component{
     constructor(props){
       super(props);
       this.state = {
-        img: props.img,
+        id: props.id,
       };
     }
+
+    sendRGroup = (r_group_id) => {
+      this.props.selectRGroupCallback(r_group_id);
+    }
+    
+    imageClick = () => {
+      this.sendRGroup(this.state.id)
+    }
+
     render(){
+      const url = 'http://127.0.0.1:5000/r-group-'
       return (
-        <img src={this.state.img} alt='R Group'/>
+        <img src={url + this.state.id} alt='R Group' onClick={this.imageClick} />
       )
     }
   }
+
+   
   
   class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        selected_r_group: 'A04',
+      };
+    }
+
+    setSelectedRGroupCallback = (r_group_id) => {
+      this.setState({selected_r_group: r_group_id}, () => {
+        console.log(this.state.selected_r_group);
+    });
+    }
+
     render() {
+      var url = 'http://127.0.0.1:5000/r-group-'
       return (
         <div className="app">
           <h1>Selected R Group:</h1>
           <div className="app-rgroup-selected">
-            <RGroupWidget img='http://127.0.0.1:5000/r-group-A01'/>
+            {console.log(this.state.selected_r_group)}
+            <RGroupWidget key={this.state.selected_r_group} id={this.state.selected_r_group} callBackFunction = {this.setSelectedRGroupCallback}/>
           </div>
           <h1>R Group Options:</h1>
           <div className="app-rgroup-row1">
-            <RGroupWidget img='http://127.0.0.1:5000/r-group-A01'/>
-            <RGroupWidget img='http://127.0.0.1:5000/r-group-A02'/>
+            <RGroupWidget key='A01' id='A01' selectRGroupCallback = {this.setSelectedRGroupCallback}/>
+            <RGroupWidget key='A02' id='A02' selectRGroupCallback = {this.setSelectedRGroupCallback}/>
           </div>
           <div className="app-rgroup-row2">
-              <RGroupWidget img='http://127.0.0.1:5000/r-group-A03'/>
-              <RGroupWidget img='http://127.0.0.1:5000/r-group-A04'/>
+            <RGroupWidget key='A03' id='A03' selectRGroupCallback = {this.setSelectedRGroupCallback}/>
+            <RGroupWidget key='A04' id='A04' selectRGroupCallback = {this.setSelectedRGroupCallback}/>
           </div>
           <div className="app-rgroup-row3">
-            <RGroupWidget img='http://127.0.0.1:5000/r-group-A05'/>
-            <RGroupWidget img='http://127.0.0.1:5000/r-group-A06'/>
+            <RGroupWidget key='A05' id='A05' selectRGroupCallback = {this.setSelectedRGroupCallback}/>
+            <RGroupWidget key='A06' id='A06' selectRGroupCallback = {this.setSelectedRGroupCallback}/>
           </div>
         </div>
       );
