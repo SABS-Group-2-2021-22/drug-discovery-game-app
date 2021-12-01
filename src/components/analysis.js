8d126ee0078994bc721cfec38892f29da3f60bdb
guidodee <guydurant@btinternet.com>
2021-12-01 14:41:13 +0000

    Added basic structure to analysis page

--- src/components/analysis.js
+++ src/components/analysis.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import "./analysis.css";
 
 class Analysis extends React.Component {
     render() {
         return (
-        <div>
-            The analysis will go here
+        <div className="analysis">
+            <div className="molecule_list">
+                Molecule list + button
+            </div>
+            <div className="comparison_graph">
+                Dylan's graphs
+            </div>
         </div>
         )
     }
