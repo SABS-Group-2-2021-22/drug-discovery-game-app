63449b261df307d9ff45be2208fc91376be76926
guidodee <guydurant@btinternet.com>
2021-12-01 15:55:37 +0000

    Bug fixes on main

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import "../index.css";
+import "./app.css";
 
 
 class RGroupWidget extends React.Component {
