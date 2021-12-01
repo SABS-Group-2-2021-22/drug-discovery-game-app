d379b6d4a4117e268c2026dd22b014fd1b29a820
guidodee <guydurant@btinternet.com>
2021-12-01 11:07:35 +0000

    Updated title

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,7 +7,7 @@ function Navigation() {
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <div className="container">
             <NavLink className="navbar-brand" to="/">
-              React Multi-Page Website
+              Drug Discovery Game
             </NavLink>
             <div>
               <ul className="navbar-nav ml-auto">
