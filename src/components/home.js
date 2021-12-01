c4b646d6e7098b4c88613a9fec6aacab4666fac4
Nele Quast <nele.quast@dtc.ox.ac.uk>
2021-12-01 10:01:56 +0000

    #11 init commit for multiple pages w./ react routes

---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcomd to DD Game!! :) </h1>
+        </div>
+    );
+}
+
+
+export default Home;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Navigation } from "./navigation";
+export { default as Home } from "./home";
+export { default as App } from "./app";
