e2335117d8728330541fbf6080e3171acf92468c
guidodee <guydurant@btinternet.com>
2021-12-08 18:31:00 +0000

    Rubbish tests for api calls - do not use in real game

--- src/components/assay.js
+++ src/components/assay.js
@@ -4,14 +4,25 @@ import "./assay.css";
 class Assay_Buttons extends React.Component {
     render () {
         return(
-        <div className="assay_button">
+        <button className="assay_button">
             {this.props.label}
-        </div>
+        </button>
         )
 
     }
 }
 class Assay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {pIC50:'No', c_mouse:'No', c_human:'No', LogD:'No', PAMPA:'No' };
+        this.triggerAllAssay = this.triggerAllAssay.bind(this);
+    }
+
+    triggerAllAssay() {
+        const base_url = 'http://127.0.0.1:5000/assays?pIC50='
+        fetch(base_url+this.state.pIC50+'&c_mouse='+this.state.c_mouse+'&c_human='+this.state.c_human+'&LogD='+this.state.LogD+'&PAMPA='+this.state.PAMPA, {method: "POST"});
+        this.setState({pIC50:'No', c_mouse:'No', c_human:'No', LogD:'No', PAMPA:'No'})
+    }
     render() {
         return (
         <div className="assay">
@@ -19,20 +30,22 @@ class Assay extends React.Component {
                  Saved molecules
             </div>
             <div className="assay_button_bar">
-                 <Assay_Buttons label="pIC50"/>
-                 <Assay_Buttons label="Clearance Mouse"/>
-                 <Assay_Buttons label="Clearance Human"/>
-                 <Assay_Buttons label="LogD"/>
-                 <Assay_Buttons label="PAMPA"/>
-                 <Assay_Buttons label="Run filters"/>
-                 <Assay_Buttons label="Calculate Descriptors"/>
+                 <button label="pIC50" onClick={() => this.setState({pIC50:'Yes'})}/>
+                 <button label="Clearance Mouse" onClick={() =>this.setState({c_mouse:'Yes'})}/>
+                 <button label="Clearance Human" onClick={() =>this.setState({c_human:'Yes'})}/>
+                 <button label="LogD" onClick={() =>this.setState({logD:'Yes'})}/>
+                 <button label="PAMPA" onClick={() =>this.setState({PAMPA:'Yes'})}/>
+                 <button label="Run filters"/>
+                 <button label="Calculate Descriptors"/>
+                 <button label="Run Assays" onClick={()=> this.triggerAllAssay()}/>
 
             </div>
             <div className="display_molecule_bar">
-                 Selected molecule
-                 + assay values 
-                 + descriptor values
-                 + filter pass/fail
+                    {this.state.pIC50}
+                    {this.state.c_mouse}
+                    {this.state.c_human}
+                    {this.state.LogD}
+                    {this.state.PAMPA}
             </div>
             
         </div>
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,20 +1,41 @@
 import React from "react";
 
-function Footer() {
-  return (
-    <div className="footer">
-      <footer class="py-3 bg-dark fixed-bottom">
-        <div class="container">
-          <p class="m-0 text-end text-white">
-          <h5>🕑: 3 weeks left</h5>
-          </p>
-          <p class="m-0 text-end text-white">
-          <h5>💰: £100,000</h5>
-          </p>
-        </div>
-      </footer>
-    </div>
-  );
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {time_money:[0,0]};
+    this.updateTimeandMoney = this.updateTimeandMoney.bind(this);
+}
+  updateTimeandMoney() {
+    const base_url = 'http://127.0.0.1:5000/update_time_money'
+    fetch(base_url)
+    .then((response) => response.json())
+    .then(time_and_money => {
+      this.setState({time_money: time_and_money})
+    })
+    .catch(err => {
+      throw Error(err.message);
+    });
+  }
+  render() {
+    return (
+      <div className="footer">
+        <footer class="py-3 bg-dark fixed-bottom">
+          <div class="container">
+            <p class="m-0 text-end text-white">
+            <h5>🕑: {this.state.time_money[1]} weeks left</h5>
+            </p>
+            <p class="m-0 text-end text-white">
+            <h5>💰: £{this.state.time_money[0]}</h5>
+            </p>
+            <button label="Update Time" onClick={()=> this.updateTimeandMoney() }></button>
+            <div>{this.state.time_money[0]}</div>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+  
 }
 
 export default Footer;
