8809f8e25d3b2d14176bbcf623e72e3fd1c2ffde
Nele Quast <nele.quast@dtc.ox.ac.uk>
2021-12-13 20:12:16 +0000

    #26 assays values are called and rendered correctly. todo: formatting

--- src/components/assay.js
+++ src/components/assay.js
@@ -20,17 +20,6 @@ class AssayPanel extends React.Component {
 }
 
 
-{/* <div className="assay_button_bar">
-                 <button label="pIC50" onClick={() => this.setState({pIC50:'Yes'})}/>
-                 <button label="Clearance Mouse" onClick={() =>this.setState({c_mouse:'Yes'})}/>
-                 <button label="Clearance Human" onClick={() =>this.setState({c_human:'Yes'})}/>
-                 <button label="LogD" onClick={() =>this.setState({logD:'Yes'})}/>
-                 <button label="PAMPA" onClick={() =>this.setState({PAMPA:'Yes'})}/>
-                 <button label="Run filters"/>
-                 <button label="Calculate Descriptors"/>
-                 <button label="Run Assays" onClick={()=> this.triggerAllAssay()}/> */}
-
-
 class Assay_Buttons extends React.Component {
     constructor(props) {
         super(props);
@@ -61,9 +50,6 @@ class MoleculeWidget extends React.Component {
             assays_have_run: (r_group_string in this.props.assay_dict),
             assay_dict: (r_group_string in this.props.assay_dict) ? this.props.assay_dict[r_group_string] : {},
         }
-        console.log('HERE ****************************************')
-        console.log(this.state.assays_have_run)
-        console.log(this.state.assay_dict)
     }
 
     sendMolecule = (r_groups) => {
@@ -75,16 +61,11 @@ class MoleculeWidget extends React.Component {
     }
 
     render() {
-        var r_group_string = this.props.r_groups[0] + this.props.r_groups[1]
-        if (r_group_string in this.props.assay_dict) {
-            this.setState({assays_have_run: true});
-            this.setState({assay_dict: this.props.assay_dict[r_group_string]}) 
-        }
         return (
             <div className='molecule-container'>
                 <div className="molecule-widget" onClick={this.imageClick} >
                     <MoleculeImage key={this.props.key} r_groups={this.props.r_groups} />
-                    {this.state.assays_have_run && <MoleculeStats assay_stats={this.state.assay_dict} ></MoleculeStats>}
+                    {(this.props.r_groups[0] + this.props.r_groups[1]) in this.props.assay_dict && <MoleculeStats assay_stats={this.props.assay_dict[this.props.r_groups[0] + this.props.r_groups[1]]} ></MoleculeStats>}
                 </div>
             </div>
         )
@@ -96,10 +77,10 @@ class MoleculeWidget extends React.Component {
 class MoleculeStats extends React.Component {
     constructor(props) {
         super(props);
+        console.log(props.assay_stats)
         this.state = {
             assay_dict: props.assay_stats,
         };
-        console.log(this.state.assay_dict)
     }
     render() {
         return (
@@ -144,7 +125,13 @@ class MoleculeList extends React.Component {
         return (
             <div className='molecule-list' >
                 {Array.from({ length: this.props.saved_mol_list.length }, (_, i) =>
-                    <MoleculeWidget key={this.props.saved_mol_list[i]} r_groups={this.props.saved_mol_list[i]} assay_dict={this.props.assay_dict} selectMoleculeCallback={this.props.selectMoleculeCallback} />)}
+                    <MoleculeWidget
+                        key={this.props.saved_mol_list[i]}
+                        r_groups={this.props.saved_mol_list[i]}
+                        assays_have_run={this.props.assays_have_run}
+                        assay_dict={this.props.assay_dict}
+                        selectMoleculeCallback={this.props.selectMoleculeCallback}
+                    />)}
             </div>
         );
     }
@@ -158,6 +145,7 @@ class Assay extends React.Component {
             selected_mol: ['A01', 'B01'],
             assay_dict: { pIC50: 'No', c_mouse: 'No', c_human: 'No', LogD: 'No', PAMPA: 'No' },
             assay_results: {},
+            assays_have_run: false,
         };
         this.getSavedMolecules();
     }
@@ -177,6 +165,7 @@ class Assay extends React.Component {
                 this.setState({ assay_results: response.assay_dict }, () => {
                     console.log(this.state.assay_results);
                 })
+                this.setState({ assays_have_run: true })
             })
             .catch(err => {
                 throw Error(err.message);
@@ -222,12 +211,9 @@ class Assay extends React.Component {
             <div className="wrapper">
                 <div className="assay">
                     <div className="molecule-chooser_bar">
-                        <MoleculeList saved_mol_list={this.state.list} assay_dict={this.state.assay_results} selectMoleculeCallback={this.setSelectedMoleculeCallback} />
+                        <MoleculeList saved_mol_list={this.state.list} assays_have_run={this.state.assays_have_run} assay_dict={this.state.assay_results} selectMoleculeCallback={this.setSelectedMoleculeCallback} />
                     </div>
-                    {/* <div className="assay_button_bar"> */}
                     <div className="assay-panel">
-                        {/* <AssayPanel /> */}
-                        {/* <div className="assay_button_bar"> */}
                         <button label="pIC50" onClick={() => this.setState({ assay_dict: { ...this.state.assay_dict, pIC50: 'Yes' } })}>pIC50</button>
                         <button label="Clearance Mouse" onClick={() => this.setState({ assay_dict: { ...this.state.assay_dict, c_mouse: 'Yes' } })}>Clearance Mouse</button>
                         <button label="Clearance Human" onClick={() => this.setState({ assay_dict: { ...this.state.assay_dict, c_human: 'Yes' } })}>Clearance Human</button>
@@ -237,7 +223,6 @@ class Assay extends React.Component {
                         <button label="Calculate Descriptors" >Calculate Descriptors</button>
                         <button label="Run Assays" onClick={this.triggerAllAssay}>Run Assays</button>
 
-                        {/* </div> */}
                     </div>
                     <div className="display_molecule_bar">
                         <MoleculeImage key={this.state.selected_mol} r_groups={this.state.selected_mol} />
