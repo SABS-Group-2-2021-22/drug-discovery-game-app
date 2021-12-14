258ab47ce7de0808c17551bca43ae56c492dd790
k-shep <92580678+k-shep@users.noreply.github.com>
2021-12-14 16:50:36 +0000

    Cleaning up code

--- src/components/results.js
+++ src/components/results.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import "./results.css";
 import styles from "./results.module.css";
-// import ChosenMol from '../drug-discovery-game-app/src/A04.png'
-// import Roche_mol from '/home/sabsr3/Drugdiscoverygame2021-2/drug-discovery-game-app/src/target_mol.png'; // Tell webpack this JS file uses this image
-//console.log(Roche_mol); // /Roche_mol.84287d09.png
-
 import { MoleculeImage } from './app';
 
 class Results extends React.Component {    
@@ -16,8 +12,6 @@ class Results extends React.Component {
         };
     };
 
-
-
     onButtonClickHandler = () => {
         window.alert('Saved')
       };
@@ -28,30 +22,29 @@ class Results extends React.Component {
 
     render() {
         console.log('Renderer started')
-        return (
-      
-          
+        return (      
         <div className ="results">
+
             {/* Roche's molecule */  }
             <div className = "real_molecule">
                 <div className="real_molecule_title">
                     <span className={styles.red_bold}>
                         Roche's molecule
-                        </span> 
-                
+                    </span> 
+  
                 </div>
                 
                 <div className="real_molecule_image_and_descriptors">
                     <div className="real_molecule_image">
                         <figure>
                         <div className="display_molecule_bar">
-                            <MoleculeImage key={this.state.Roche_mol} r_groups={this.state.Roche_mol} />
+                            <MoleculeImage 
+                            key={this.state.Roche_mol} 
+                            r_groups={this.state.Roche_mol} 
+                            alt="Roche_molecule" />    {/* Alt text not working */}
                         </div>
-                            {/* <p> <img src={Roche_mol}
-                                // width="400"
-                                // height="300"
-                                alt="Roche_molecule" /> </p> */}
-                            <figcaption>Below is the molecule that Roche chose
+                            <figcaption>
+                                Below is the molecule that Roche chose
                             </figcaption>
                         </figure>
 
@@ -69,6 +62,8 @@ class Results extends React.Component {
                 </div>
 
             </div>
+
+
              {/* Chosen molecule */  }
             <div className = "chosen_molecule">
                  <div className="chosen_molecule_title">
@@ -83,14 +78,8 @@ class Results extends React.Component {
                                 <div className="display_molecule_bar">
                                     <MoleculeImage key={this.state.selected_mol} r_groups={this.state.selected_mol} />
                                  </div>
-                                
-                                {/* PUT SPIDER PLOT HERE WHERE IMAGE THING WAS!!!! */}
-
-                                {/* <p> <img src={Roche_mol} 
-                                    // width="400"
-                                    // height="300"
-                                    alt="Roche_molecule" /> </p> */}
-                                <figcaption>Below is the molecule that you chose - need to change image
+                                <figcaption>
+                                    Below is the molecule that you chose - need to change image
                                 </figcaption>
                             </figure>
 
@@ -116,17 +105,15 @@ class Results extends React.Component {
                     <span className={styles.red_bold}>
                         Spider Plot
                         </span> 
-                    
-                    {/* More text */}
-                
                 </div>
                 <div className="spider_plot_image">
-
                 <figure>
-                    {/* <p> <img src={Roche_mol}
+                    {/* PUT SPIDER PLOT HERE WHERE IMAGE THING WAS/IS!!!! */}
+
+                    {/* <p> <img src={IMAGE_NAME}
                         // width="400"
                         // height="300"
-                        alt="Roche_molecule" /> </p> */}
+                        alt="molecule" /> </p> */}
                     <figcaption>Below is a Spider Plot
                     </figcaption>
                 </figure>
@@ -150,83 +137,4 @@ class Results extends React.Component {
     }
 }
 
-
-// class App extends React.Component{
-
-//     onButtonClickHandler = () => {
-//       window.alert('Hi')
-//     };
-  
-//     render(){ 
-//       return(<div className="App">
-//         <button onClick={this.onButtonClickHandler}>Enter</button>
-//       </div>);
-  
-//     }
-//   }  
-
-// class ControlPanel extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-//         current_r_groups: props.current_r_groups,
-//       };
-//     }
-  
-//     saveMolecule = () => {
-//       const base_url = 'http://127.0.0.1:5000/save'
-//       fetch(base_url + '?r1=' + this.state.current_r_groups[0] 
-//                      + '&r2=' + this.state.current_r_groups[1], 
-//                      {method: 'POST'})
-//         }
-  
-//     render() {
-//       const { text } = this.state;
-//       return (
-//         <div className="control-panel">
-//           <button>Clear</button>
-//           <button onClick={this.saveMolecule}>Save</button>
-//           <button>Assay</button>
-//         </div>
-//       );
-//     }
-//   }
-
-
-// export {MoleculeImage, } */ Is this needed */
 export default Results;
-
-// import logo from '/home/sabsr3/Drugdiscoverygame2021-2/drug-discovery-game-app/src/A04.png'; // Tell webpack this JS file uses this image
-
-// console.log(logo); // /logo.84287d09.png
-
-// function Header() {
-//   // Import result is the URL of your image
-//   return <img src={logo} alt="Logo" />;
-// }
-
-// export default Header;
-
-
-
-{/* <figure>
-<p> <img src="target_mol.png"
-    width="136"
-    height="200"
-    alt="Eiffel tower" /> </p>
-<figcaption>Scale model of the
-    Eiffel tower in
-    Parc Mini-France
-</figcaption>
-</figure>
-
-figure {
-float:right;
-width: 30%;
-text-align: center;
-font-style: italic;
-font-size: smaller;
-text-indent: 0;
-border: thin silver solid;
-margin: 0.5em;
-padding: 0.5em;} */}
