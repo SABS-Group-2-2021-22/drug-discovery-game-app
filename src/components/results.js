3af2ef4d347b898b876b22ada8cf846f880d775b
DylanAdlard <dylan.adlard@lmh.ox.ac.uk>
2021-12-15 18:39:02 +0000

    #40 completed spider

--- src/components/results.js
+++ src/components/results.js
@@ -8,13 +8,13 @@ class Results extends React.Component {
           <div className="results">
             <div className="real_molecule">Roche's Molecule</div>
             <div className="chosen_molecule">Final Molecule</div>
-            <div className="spider_plot">Spider Plot</div>
+            <div className="spider_plot">
+                <SpiderPlot />
+            </div>
             <div className="explanation_results">
               Explanation of what went right/wrong
             </div>
-            <div>
-              <SpiderPlot />
-            </div>
+
           </div>
         );
     }
