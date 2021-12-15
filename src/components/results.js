229d4eaed748c50eb46f2df08a7176e4505b0c12
DylanAdlard <dylan.adlard@lmh.ox.ac.uk>
2021-12-15 18:33:06 +0000

    #40 implemented spiderplot

--- src/components/results.js
+++ src/components/results.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import "./results.css";
+import SpiderPlot from "./SpiderPlot.js";
 
 class Results extends React.Component {
     render() {
         return (
-        <div className ="results">
-            <div className = "real_molecule">
-                Roche's Molecule
+          <div className="results">
+            <div className="real_molecule">Roche's Molecule</div>
+            <div className="chosen_molecule">Final Molecule</div>
+            <div className="spider_plot">Spider Plot</div>
+            <div className="explanation_results">
+              Explanation of what went right/wrong
             </div>
-            <div className = "chosen_molecule">
-                Final Molecule
+            <div>
+              <SpiderPlot />
             </div>
-            <div className = "spider_plot">
-                Spider Plot
-            </div>
-            <div className = "explanation_results">
-                Explanation of what went right/wrong
-            </div>
-        </div>
-        )
+          </div>
+        );
     }
 }
 
