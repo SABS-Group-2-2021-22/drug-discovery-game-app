f1790d3f70fb12a36958672007e4ca7145c7dbd0
k-shep <92580678+k-shep@users.noreply.github.com>
2021-12-15 16:27:41 +0000

    #17 Changes as result of Nele's review to simplify files

--- src/components/results.js
+++ src/components/results.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import "./results.css";
-import styles from "./results.module.css";
 import { MoleculeImage } from './app';
 
-class Results extends React.Component {    
+class Results extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -12,127 +11,136 @@ class Results extends React.Component {
         };
     };
 
+    fetchChosenMolecule() {
+        const base_url = 'http://127.0.0.1:5000/chosenmolecule'
+        fetch(base_url)
+            .then((response) => response.json())
+            .then(chosen_mol => {
+                this.setState({ final: 'A0' + chosen_mol.chosen_mol }) /* Remove 'A0' when have changed analysis page st */
+                console.log(chosen_mol)
+                console.log(this.state.final)
+            })
+            .catch(err => {
+                throw Error(err.message);
+            });
+    };
+
     onButtonClickHandler = () => {
         window.alert('Saved')
-      };
+    };
 
     endclick = () => {
         window.alert('Thank you for playing')
-      };
+    };
 
     render() {
         console.log('Renderer started')
-        return (      
-        <div className ="results">
+        return (
+            <div className="results">
 
-            {/* Roche's molecule */  }
-            <div className = "real_molecule">
-                <div className="real_molecule_title">
-                    <span className={styles.red_bold}>
+                {/* Roche's molecule */}
+                <div className="real-molecule">
+                    <div className="title">
                         Roche's molecule
-                    </span> 
-  
-                </div>
-                
-                <div className="real_molecule_image_and_descriptors">
-                    <div className="real_molecule_image">
-                        <figure>
-                        <div className="display_molecule_bar">
-                            <MoleculeImage 
-                            key={this.state.Roche_mol} 
-                            r_groups={this.state.Roche_mol} 
-                            alt="Roche_molecule" />    {/* Alt text not working */}
+                    </div>
+
+                    <div className="molecule-image-and-descriptors">
+                        <div className="molecule-image">
+                            <figure>
+                                <div className="display_molecule_bar">
+                                    <MoleculeImage
+                                        key={this.state.Roche_mol}
+                                        r_groups={this.state.Roche_mol}
+                                        alt="Roche_molecule" />    {/* Alt text not working */}
+                                </div>
+                                <figcaption>
+                                    Below is the molecule that Roche chose
+                                </figcaption>
+                            </figure>
+
                         </div>
-                            <figcaption>
-                                Below is the molecule that Roche chose
-                            </figcaption>
-                        </figure>
 
-                    </div>
+                        <div class="container" className="molecule-descriptors">
+                            <div className="row">Descriptors:</div>
+                            <div className="row">pIC50: number units </div>
+                            <div className="row">logD: number units</div>
+                            <div className="row">Mouse clearance: descriptor</div>
+                            <div className="row">Human clearane: descriptor</div>
+                            <div className="row">Permeability: descriptor</div>
+                        </div>
 
-                    <div className="real_molecule_descriptors">
-                    Descriptors:
-                    {"\n"}pIC50: number units
-                    {"\n"}logD: number units
-                    {"\n"}Mouse clearance: descriptor
-                    {"\n"}Human clearane: descriptor
-                    {"\n"}Permeability: descriptor
+
+
                     </div>
 
                 </div>
 
-            </div>
 
-
-             {/* Chosen molecule */  }
-            <div className = "chosen_molecule">
-                 <div className="chosen_molecule_title">
-                    <span className={styles.red_bold}>
+                {/* Chosen molecule */}
+                <div className="chosen-molecule">
+                    <div className="title">
                         Final molecule
-                        </span> 
-                </div>
+                    </div>
 
-                <div className="chosen_molecule_image_and_descriptors">
-                        <div className="chosen_molecule_image">
+                    <div className="molecule-image-and-descriptors">
+                        <div className="molecule-image">
                             <figure>
                                 <div className="display_molecule_bar">
                                     <MoleculeImage key={this.state.final} r_groups={this.state.final} />
-                                 </div>
+                                </div>
                                 <figcaption>
                                     Below is the molecule that you chose - need to change image
                                 </figcaption>
                             </figure>
 
-                    </div>
+                        </div>
 
-                    <div className="chosen_molecule_descriptors">
-                    Descriptors:
-                    {"\n"}pIC50: number units
-                    {"\n"}logD: number units
-                    {"\n"}Mouse clearance: descriptor
-                    {"\n"}Human clearane: descriptor
-                    {"\n"}Permeability: descriptor
+                        <div class="container" className="molecule-descriptors">
+                            <div className="row">Descriptors:</div>
+                            <div className="row">pIC50: number units</div>
+                            <div className="row">logD: number units</div>
+                            <div className="row">Mouse clearance: descriptor</div>
+                            <div className="row">Human clearane: descriptor</div>
+                            <div className="row">Permeability: descriptor</div>
+                        </div>
+
                     </div>
 
                 </div>
 
-            </div>
-            
 
-             {/* Spider plot */  }
-            <div className = "spider_plot">
-            <div className="spider_plot_title">
-                    <span className={styles.red_bold}>
+                {/* Spider plot */}
+                <div className="spider-plot">
+                    <div className="title">
                         Spider Plot
-                        </span> 
-                </div>
-                <div className="spider_plot_image">
-                <figure>
-                    {/* PUT SPIDER PLOT HERE WHERE IMAGE THING WAS/IS!!!! */}
+                    </div>
+                    <div className="spider-plot-image">
+                        <figure>
+                            {/* PUT SPIDER PLOT HERE WHERE IMAGE THING WAS/IS!!!! */}
 
-                    {/* <p> <img src={IMAGE_NAME}
+                            {/* <p> <img src={IMAGE_NAME}
                         // width="400"
                         // height="300"
                         alt="molecule" /> </p> */}
-                    <figcaption>Below is a Spider Plot
-                    </figcaption>
-                </figure>
+                            <figcaption>Below is a Spider Plot
+                            </figcaption>
+                        </figure>
+                    </div>
+
+
                 </div>
 
+                {/* Explanation of results */}
+                <div className="explanation-results">
+                    Explanation of what went right/wrong
 
-            </div>
+                    <div className="control-panel">
+                        <button onClick={this.endclick}>End</button>
+                        <button onClick={this.onButtonClickHandler}>Save</button>
+                    </div>
 
-            {/* Explanation of results */  }
-            <div className = "explanation_results">
-                Explanation of what went right/wrong
-                
-                <div className="control-panel">
-                <button onClick={this.endclick}>End</button>
-                <button onClick={this.onButtonClickHandler}>Save</button>
                 </div>
-
             </div>
-        </div>
         )
     }
 }
