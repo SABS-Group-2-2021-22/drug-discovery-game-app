d0ea553a213094503d508436ecce621dd056c82a
Nele Quast <nele.quast@dtc.ox.ac.uk>
2021-12-16 01:35:22 +0000

    #46 transition to results fixed

--- src/components/results.js
+++ src/components/results.js
@@ -2,22 +2,52 @@ import React from 'react';
 import "./results.css";
 import SpiderPlot from "./SpiderPlot.js";
 
+import { MoleculeImage } from './app'
+
 class Results extends React.Component {
-    render() {
-        return (
-          <div className="results">
-            <div className="real_molecule">Roche's Molecule</div>
-            <div className="chosen_molecule">Final Molecule</div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      chosen_mol: undefined,
+      roche_mol: ['A05', 'B07']
+    };
+    this.getChosenMolecule();
+  }
+  getChosenMolecule = () => {
+    const base_url = 'http://127.0.0.1:5000/chosenmolecule'
+    fetch(base_url)
+      .then((response) => response.json())
+      .then(response => {
+        this.setState({ chosen_mol: response.chosen_mol })
+      })
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="results">
+          <div className='molecule-images'>
+            <div className="real_molecule">Roche's Molecule
+              <MoleculeImage key={this.state.roche_mol} r_groups={this.state.roche_mol} />
+
+            </div>
+            <div className="chosen_molecule">Final Molecule
+              {this.state.chosen_mol !== undefined ?
+                <MoleculeImage key={this.state.chosen_mol} r_groups={this.state.chosen_mol} />
+                : 'img'}
+            </div>
+          </div>
+          <div className='molecule-results-analysis'>
             <div className="spider_plot">
-                <SpiderPlot />
+              <SpiderPlot />
             </div>
             <div className="explanation_results">
               Explanation of what went right/wrong
             </div>
-
           </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Results;
