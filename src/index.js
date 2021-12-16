d87991aa350a668cb96c1bda07b0a39fe76dc239
Nele Quast <nele.quast@dtc.ox.ac.uk>
2021-12-16 19:14:22 +0000

    #42 time an dmoney update implemented

--- src/index.js
+++ src/index.js
@@ -9,23 +9,53 @@ import {
   Home,
   App,
   Assay,
-  Analysis, 
+  Analysis,
   Results,
   Footer,
 } from "./components";
 
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      money: undefined,
+      time: undefined,
+      };
+      this.updateTimeAndMoney();
+}
+  updateTimeAndMoney = () => {
+    const base_url = 'http://127.0.0.1:5000/update_time_money'
+    // const base_url = 'http://drug-discovery-game-backend.herokuapp.com/update_time_money'
+    fetch(base_url)
+      .then((response) => response.json())
+      .then(time_and_money => {
+        this.setState({ money: time_and_money[0] })
+        this.setState({ time: time_and_money[1] })
+      })
+      .catch(err => {
+        throw Error(err.message);
+      });
+  }
+
+  render() {
+    return (
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/app" element={<App />} />
+          <Route path="/assay" element={<Assay updateTimeAndMoneyCallback={this.updateTimeAndMoney}/>} />
+          <Route path="/analysis" element={<Analysis />} />
+          <Route path="/results" element={<Results />} />
+        </Routes>
+        <Footer time={this.state.time} money={this.state.money}/>
+      </Router>
+    )
+  }
+}
+
 ReactDOM.render(
-  <Router>
-    <Navigation />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/app" element={<App />} />
-      <Route path="/assay" element={<Assay/>} />
-      <Route path="/analysis" element={<Analysis/>} />
-      <Route path="/results" element={<Results/>} />
-    </Routes>
-    <Footer />
-  </Router>,
+  <Index/>,
 
   document.getElementById("root")
 );
