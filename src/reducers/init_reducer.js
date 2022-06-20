bfe20b82ff318f19d0fd7f7705e18a176e2d02a0
DylanAdlard <dylan.adlard@lmh.ox.ac.uk>
2022-06-20 23:16:14 +0100

    docstrings for reducers

--- src/reducers/init_reducer.js
+++ src/reducers/init_reducer.js
@@ -2,6 +2,12 @@ const initialState = {
   all_r_groups: [],
 };
 
+/**
+ * Updates the store with r groups and Roche's molecule after an initial api dispatch
+ * @param {state object} state the intial empty r group state
+ * @param {action} action
+ * @returns {state} update all_r_group state and Roche molecule state
+ */
 export function initReducer(state = initialState, action) {
   switch (action.type) {
     case "FETCH_R_GROUP_SUCCEEDED": {
--- src/reducers/login_reducer.js
+++ src/reducers/login_reducer.js
@@ -1,29 +1,35 @@
 import { userConstants } from "../constants";
 
 // Check if user is already logged in
-let user = JSON.parse(localStorage.getItem('user'));
-const initialState = user ? { loggedIn: true, user, username: {} } : {} ;
+let user = JSON.parse(localStorage.getItem("user"));
+const initialState = user ? { loggedIn: true, user, username: {} } : {};
 
+/**
+ * Controls login store state changes for logginIn, loggedIn, and user
+ * @param {state object} state the logginIn, loggedIn, and user states
+ * @param {action} action actions that update loggingIn, loggedIn, and user states
+ * @returns {state} updated logginIn, loggedIn, and user states
+ */
 export function loginReducer(state = initialState, action) {
-    switch (action.type) {
-        case userConstants.LOGIN_REQUEST:
-            return {
-                loggingIn: true,
-                user: action.user
-            };
-        case userConstants.LOGIN_SUCCESS:
-            return {
-                loggedIn: true,
-                user: action.user
-            };
-        case userConstants.LOGIN_FAILURE:
-            return {};
-        case userConstants.LOGOUT:
-            return {
-                loggedIn: false,
-                user: action.user
-            };
-        default:
-            return state
-    }
+  switch (action.type) {
+    case userConstants.LOGIN_REQUEST:
+      return {
+        loggingIn: true,
+        user: action.user,
+      };
+    case userConstants.LOGIN_SUCCESS:
+      return {
+        loggedIn: true,
+        user: action.user,
+      };
+    case userConstants.LOGIN_FAILURE:
+      return {};
+    case userConstants.LOGOUT:
+      return {
+        loggedIn: false,
+        user: action.user,
+      };
+    default:
+      return state;
+  }
 }
