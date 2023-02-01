1e7bf54c73e441607f1b43640716fc634b8715d7
adpunt <apunt@hmc.edu>
2023-02-01 14:51:03 +0000

    Reformating css layout of docking page

--- src/components/docking/docking.js
+++ src/components/docking/docking.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { initActions, selectorActions, gameActions } from "../../actions";
 import MoleculeList from "../assay/molecule_list.js";
+import Molstar from "molstar-react";
 
 class Docking extends React.Component {
   constructor(props) {
@@ -11,18 +12,27 @@ class Docking extends React.Component {
   }
 
   render() {
+    let file = {filestring:"./1e7i.pdb", type:"pdb"}
+    // https://files.rcsb.org/view/
+    // let url = "http://localhost:5000/docking"
+    let url = "http://localhost:5000/docking-6lu7.pdb"
+    console.log(file.filestring)
     return (
       <div className="wrapper">
-        <div className="docking">
+        <div className="docking-elements">
           <div className="final-molecule-bar">
             <MoleculeList />
           </div>
-          <div className="dockingtitle">Docking Viewer</div>
-          <div className="control-panel">
-            <div className="analysis-button">
-              <Link to="/analysis">
-                <button>Analysis</button>
-              </Link>
+          <div className="docking-and-button">
+            <div className="molstar">
+              <Molstar url={url}/>
+            </div>
+            <div className="control-panel">
+              <div className="analysis-button">
+                <Link to="/analysis">
+                  <button>Analysis</button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
