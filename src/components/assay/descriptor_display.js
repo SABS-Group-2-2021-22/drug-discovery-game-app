7e471924262e18cd6d03386177c87674b47675ba
Nele Quast <nele.pauline@gmail.com>
2023-02-02 18:49:14 +0000

    #249 clean up comneted out code

--- src/components/assay/descriptor_display.js
+++ src/components/assay/descriptor_display.js
@@ -5,9 +5,6 @@ import { connect } from "react-redux";
 class Descriptors extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   hover: "descr",
-    // };
   }
 
   render() {
