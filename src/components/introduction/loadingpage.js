251242a82f33f2987aad443787d5aa1ebe3d4964
Nele Quast <nele.pauline@gmail.com>
2023-02-06 18:49:41 +0000

    fixed intro text size

--- src/components/introduction/loadingpage.js
+++ src/components/introduction/loadingpage.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./loadingpage.css";
+// import "./loadingpage.css";
+import "./introduction.css";
 import { Link } from "react-router-dom";
 import pymolpic from "../../assets/pymolMMP12.png";
 import { connect } from "react-redux";
