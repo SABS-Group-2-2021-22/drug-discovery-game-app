150e5fb55c4ef7e23fddc9dd0287c4513bce0ff4
anissa218 <73247232+anissa218@users.noreply.github.com>
2023-03-02 10:34:37 +0000

    fix styling

--- src/components/analysis/the_plot.js
+++ src/components/analysis/the_plot.js
@@ -156,9 +156,9 @@ class ThePlot extends React.Component {
         />
         <div className="plot-button-bigpanel">
         <div className="plot-button">
-        <Link to="/assay">
-            <button>{'<< Test <<'}</button>
-        </Link>
+          <Link to="/assay">
+              <button>{'<< Test <<'}</button>
+          </Link>
         </div>
         <div className="plot-button-panel">
           <div className="plot-button-row">
