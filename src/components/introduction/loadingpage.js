1d997e414d0c00b2851d077ac072f9f68892b871
SimiaoZhao <Zhaosimiao1@gmail.com>
2023-03-07 15:58:52 +0000

    avoid jump

--- src/components/introduction/loadingpage.js
+++ src/components/introduction/loadingpage.js
@@ -26,10 +26,12 @@ class Loadingpage extends React.Component {
                 </Link> 
                 </div>
                 ):
-                (<div className="loadingpage">  
-                        <div style={{ display: 'block',
-                  width: 400, padding: 10}}>
-                <ProgressBar animated variant="dark" now={this.props.num} label={`Loading ${this.props.num}%`} />
+                ( 
+                <div className="loadingpage">  
+                  <p>Loading...</p>
+                  <div style={{ display: 'block',
+                    width: 400, padding: 10}}>
+                  <ProgressBar animated variant="dark" now={this.props.num} label={`${this.props.num}%`} />
                 </div> 
                 </div>)
         }
