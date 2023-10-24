23641e890ae18806e225910ae5fd98380787e949
James-Broster <h.broster1243@hotmail.com>
2023-10-24 15:24:06 +0100

    294 added colour

--- src/components/analysis/descriptor_display.js
+++ src/components/analysis/descriptor_display.js
@@ -7,54 +7,97 @@ class Descriptors extends React.Component {
     super(props);
   }
 
+
   render() {
+
+    //set *varible*_value to its corresponding value for each varibale, called from their respective classes
+    const HA_value = this.props.mols[this.props.mol_id].data.descriptors.HA;
+    const MW_value = Number(this.props.saved_mols[this.props.mol_id].data.descriptors.MW).toFixed(1);
+    const TPSA_value = Number(this.props.saved_mols[this.props.mol_id].data.descriptors.TPSA).toFixed(1);
+    const h_acc_value = this.props.saved_mols[this.props.mol_id].data.descriptors.h_acc;
+    const h_don_value = this.props.saved_mols[this.props.mol_id].data.descriptors.h_don;
+    const LogP_value = Number(this.props.saved_mols[this.props.mol_id].data.descriptors.logP).toFixed(1);
+
+    //set all styles to red (these will be changed based on thresholds set in next block of code)
+    let HA_color = "red";
+    let MW_color = "red";
+    let TPSA_color = "green";
+    let h_acc_color = "green";
+    let h_don_color = "green";
+    let LogP_color = "green";
+    
+    //change color based on the thresholds for if value is good, bad or neutral
+    if (HA_value >= 6) {
+      HA_color = "green";
+    } else if (HA_value >= 4) {
+      HA_color = "yellow";
+    }
+  
+    if (MW_value >= 700) {
+      MW_color = "green";
+    } else if (MW_value >= 500) {
+      MW_color = "yellow";
+    }
+  
+    if (TPSA_value >= 140) {
+      TPSA_color = "red";
+    } else if (TPSA_value >= 90) {
+      TPSA_color = "yellow";
+    }
+  
+    if (h_acc_value >= 10) {
+      h_acc_color = "red";
+    } else if (h_acc_value >= 3) {
+      h_acc_color = "yellow";
+    }
+  
+    if (h_don_value >= 5) {
+      h_don_color = "red";
+    } else if (h_don_value >= 3) {
+      h_don_color = "yellow";
+    }
+  
+    if (LogP_value >= 5) {
+      LogP_color = "red";
+    } else if (LogP_value >= 3) {
+      LogP_color = "yellow";
+    }
+  
+    //return the variable_values in with the correct colours based on thresholds above
     return (
       <div class="container" className="descriptor-stats">
         <div class="row" className="stats-type-header">
           Molecule Descriptors:
         </div>
-        <div class="row">
-          HA: {this.props.saved_mols[this.props.mol_id].data.descriptors.HA}
+        <div class="row" style={{ color: HA_color }}>
+          HA: {HA_value}
         </div>
-        <div class="row">
-          MW:{" "}
-          {Number(
-            this.props.saved_mols[this.props.mol_id].data.descriptors.MW
-          ).toFixed(1)}  Da
+        <div class="row" style={{ color: MW_color }}>
+          MW: {MW_value} Da
         </div>
-        <div class="row">
-          TPSA:{" "}
-          {Number(
-            this.props.saved_mols[this.props.mol_id].data.descriptors.TPSA
-          ).toFixed(1)} {"Å\u00b2"}
+        <div class="row" style={{ color: TPSA_color }}>
+          TPSA: {TPSA_value} {"Å\u00b2"}
         </div>
-        <div class="row">
-          H Acc.:{" "}
-          {this.props.saved_mols[this.props.mol_id].data.descriptors.h_acc}
+        <div class="row" style={{ color: h_acc_color }}>
+          H Acc.: {h_acc_value}
         </div>
-        <div class="row">
-          H Don.:{" "}
-          {this.props.saved_mols[this.props.mol_id].data.descriptors.h_don}
+        <div class="row" style={{ color: h_don_color }}>
+          H Don.: {h_don_value}
         </div>
-        <div class="row">
-          LogP:{" "}
-          {Number(
-            this.props.saved_mols[this.props.mol_id].data.descriptors.logP
-          ).toFixed(1)}
-        </div>
-        <div class="row">
-          Rings:{" "}
-          {this.props.saved_mols[this.props.mol_id].data.descriptors.rings}
+        <div class="row" style={{ color: LogP_color }}>
+          LogP: {LogP_value}
         </div>
       </div>
     );
-  };
+  }
 }
 
-function mapStateToProps(state) {
-  return {
-    saved_mols: state.assay.saved_mols
-  };
-}
 
-export default connect(mapStateToProps)(Descriptors);
+
+  function mapStateToProps(state) {
+    return {
+      saved_mols: state.assay.saved_mols
+    };
+  }
+  
+  export default connect(mapStateToProps)(Descriptors);
