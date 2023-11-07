007f77257ece8d8e3ba9fd4d15b1183854faa321
TomKWise <tomkennedywise@gmail.com>
2023-11-07 20:02:50 +0000

    #416 416-add-assay-data-to-design-page

--- src/components/builder/molecule_widget.js
+++ src/components/builder/molecule_widget.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./builder.css";
+import { connect } from "react-redux";
 import MoleculeImage from "./molecule_image.js";
+import Assays from "../assay/assay_display.js";
+
 
 class MoleculeWidget extends React.Component {
   constructor(props) {
@@ -13,10 +16,23 @@ class MoleculeWidget extends React.Component {
         <div className="molecule-card">
           <MoleculeImage mol_id={this.props.mol_id} />
           {this.props.mol_id}
-      </div>
+          <div className = "assay_text">
+            {this.props.saved_mols[this.props.mol_id].data.assays_run
+            .drug_props && <Assays mol_id={this.props.mol_id} />}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default MoleculeWidget;
+function mapStateToProps(state) {
+  return {
+    //each assay has to be individually accessed from 'assays_run' to force a component update
+    saved_mols: state.assay.saved_mols,
+  };
+}
+
+export default connect(mapStateToProps)(MoleculeWidget);
+
+
