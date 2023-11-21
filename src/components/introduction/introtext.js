f26da73d78d4ca18ca559f8b77ac44bd580c5dca
TomKWise <tomkennedywise@gmail.com>
2023-11-21 08:19:05 +0000

    433-correct-intro-links-etc

--- src/components/introduction/introtext.js
+++ src/components/introduction/introtext.js
@@ -17,8 +17,7 @@ render()
         <div className="introtext">
          <p>There are 4 stages in the drug discovery process, namely:
          {"\n"}1. <b>Design</b> and <b>make</b> potential lead compounds. Compound design can be guided 
-         by observation of <a href="https://en.wikipedia.org/wiki/Lipinski%27s_rule_of_five#cite_note-Lipinski_2001-1">Lipinski's rule of five</a> as well as other metrics for quantifying desirable
-          <a href="https://en.wikipedia.org/wiki/ADME"> ADMET</a> properties such as <a href="https://en.wikipedia.org/wiki/Polar_surface_area">TPSA</a> and <a href="https://en.wikipedia.org/wiki/Partition_coefficient#Partition_coefficient_and_log_P">LogP</a>.
+         by observation of <a href="https://en.wikipedia.org/wiki/Lipinski%27s_rule_of_five#cite_note-Lipinski_2001-1" target="_blank">Lipinski's rule of five</a> as well as other metrics for quantifying desirable <a href="https://en.wikipedia.org/wiki/ADME" target="_blank">ADMET</a> properties such as <a href="https://en.wikipedia.org/wiki/Polar_surface_area" target="_blank">TPSA</a> and <a href="https://en.wikipedia.org/wiki/Partition_coefficient#Partition_coefficient_and_log_P" target="_blank">LogP</a>.
          {"\n"}2. <b>Test</b>: Perform assays (experiments) and run calculations to determine the properties 
          of the molecules you have designed. With this additional information, you can then go
           back and develop improved compounds.
@@ -38,7 +37,7 @@ render()
 
         {this.props.id === 1 &&
         <div className="introtext">
-        <p>Matrix metalloproteinase 12 (<a href="https://en.wikipedia.org/wiki/Matrix_metallopeptidase_12">MMP-12</a>) is an enzyme implicated in emphysema and asthma. It has been identified as a possible drug target.
+        <p>Matrix metalloproteinase 12 (<a href="https://en.wikipedia.org/wiki/Matrix_metallopeptidase_12" target="_blank">MMP-12</a>) is an enzyme implicated in emphysema and asthma. It has been identified as a possible drug target.
         {"\n"}  {"\n"}  MMP-12 is made by immune cells in the lungs, and it is thought that it can cause damage and inflammation in the lungs.
         {"\n"}  {"\n"} You are going to try to design a compound with desired properties that inhibits MMP-12 in the body (including the lungs).
       </p>
@@ -49,10 +48,10 @@ render()
         <p>The optimal properties of a certain drug depend on the specific condition you are trying to treat.
         {"\n"}  {"\n"}
         The target compound profile or TCP (in terms of desired properties) for MMP-12 is indicated below:
-        {"\n"}  - Good lipophilicity (i.e. <a href="https://en.wikipedia.org/wiki/Partition_coefficient#:~:text=The%20distribution%20coefficient%2C%20log%20D%2C%20is%20the%20ratio,log%20P%20for%20non-ionizable%20compounds%20at%20any%20pH">LogD</a> ≥ 1 at neutral pH),
-        {"\n"}  - Medium to high permeability ( <a href="https://en.wikipedia.org/wiki/Parallel_artificial_membrane_permeability_assay#:~:text=In%20medicinal%20chemistry%2C%20parallel%20artificial%20membrane%20permeability%20assay,-infused%20artificial%20membrane%20into%20an%20acceptor%20compartment.%20">PAMPA</a>),
-        {"\n"}  -  Good metabolic stability (low <a href="https://en.wikipedia.org/wiki/Clearance_(pharmacology)">clearance</a>) in mouse and human, and
-        {"\n"}  -  High potency (i.e. <a href="https://www.collaborativedrug.com/cdd-blog/what-is-pic50-2#:~:text=Simply%20stated%2C%20pIC50%20is%20the,is%20a%20pIC50%20of%209">pIC<sub>50</sub></a> ≥ 6).
+        {"\n"}  - Good lipophilicity (i.e. <a href="https://en.wikipedia.org/wiki/Partition_coefficient#:~:text=The%20distribution%20coefficient%2C%20log%20D%2C%20is%20the%20ratio,log%20P%20for%20non-ionizable%20compounds%20at%20any%20pH" target="_blank">LogD</a> ≥ 1 at neutral pH),
+        {"\n"}  - Medium to high permeability (<a href="https://en.wikipedia.org/wiki/Parallel_artificial_membrane_permeability_assay#:~:text=In%20medicinal%20chemistry%2C%20parallel%20artificial%20membrane%20permeability%20assay,-infused%20artificial%20membrane%20into%20an%20acceptor%20compartment.%20" target="_blank">PAMPA</a>),
+        {"\n"}  -  Good metabolic stability (low <a href="https://en.wikipedia.org/wiki/Clearance_(pharmacology)" target="_blank">clearance</a>) in mouse and human, and
+        {"\n"}  -  High potency (i.e. <a href="https://www.collaborativedrug.com/cdd-blog/what-is-pic50-2#:~:text=Simply%20stated%2C%20pIC50%20is%20the,is%20a%20pIC50%20of%209" target="_blank">pIC<sub>50</sub></a> ≥ 6).
       </p>
       </div>}
 
