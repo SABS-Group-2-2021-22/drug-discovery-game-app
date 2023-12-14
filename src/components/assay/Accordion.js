87c30273fdfc286a6c94d2577f5b1319b00bdd9d
King Ifashe <ifasheking@gmail.com>
2023-12-14 14:15:21 +0000

    Added accordion to assay page

--- src/components/assay/Accordion.js
+++ src/components/assay/Accordion.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const Accordion = ({ title, children }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   return (
     <div className="accordion">
       <div className="accordion-title" onClick={() => setIsOpen(!isOpen)}>
         {title}
+        <span className={`accordion-arrow ${isOpen ? 'open' : 'closed'}`}></span>
       </div>
       {isOpen && <div className="accordion-content">{children}</div>}
     </div>
