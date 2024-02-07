47e32e5b483b9869d71dd7c22dce1464bbf4c350
TomKWise <tomkennedywise@gmail.com>
2024-02-07 17:24:39 +0000

    Made some changes to table etc

--- src/components/analysis/AssayDataTable.js
+++ src/components/analysis/AssayDataTable.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import MoleculeImage from "./molecule_image.js"; 
+import MoleculeImage from "./molecule_image.js";
 import "./analysis.css";
 
 const AssayDataTable = ({ saved_mols }) => {
@@ -11,18 +11,41 @@ const AssayDataTable = ({ saved_mols }) => {
   };
 
   const renderRows = () => {
-    return Object.keys(saved_mols).flatMap(mol_id => {
+    // Start by defining the target compound profile row
+    const targetProfileRow = [
+      <tr key="target-profile" className="bold-separator">
+        <td>Target compound profile</td>
+        <td>Low (&lt; 5.6 mL/min)</td>
+        <td>Low (&lt; 12 mL/min)</td>
+        <td>&ge; 1.00</td>
+        <td>Medium to High</td>
+        <td>&ge; 6.0</td>
+      </tr>
+    ];
+
+    // Then map over saved_mols to create rows for each molecule
+    const moleculeRows = Object.keys(saved_mols).flatMap(mol_id => {
       const molecule = saved_mols[mol_id];
       const assaysRun = molecule.data.assays_run;
       const isExpanded = expandedRow === mol_id;
       return [
         <tr key={`row-${mol_id}`} onClick={() => toggleRowExpansion(mol_id)} style={{ cursor: 'pointer' }}>
           <td>{mol_id}</td>
-          <td>{assaysRun.clearance_mouse ? molecule.data.drug_props.clearance_mouse : 'N/A'}</td>
-          <td>{assaysRun.clearance_human ? molecule.data.drug_props.clearance_human : 'N/A'}</td>
-          <td>{assaysRun.logd ? molecule.data.drug_props.logd : 'N/A'}</td>
-          <td>{assaysRun.pampa ? molecule.data.drug_props.pampa : 'N/A'}</td>
-          <td>{assaysRun.pIC50 ? molecule.data.drug_props.pic50 : 'N/A'}</td>
+          <td style={{ color: assaysRun.clearance_mouse ? (molecule.data.drug_props.clearance_mouse === "low (< 5.6)" ? 'green' : 'red') : 'black' }}>
+            {assaysRun.clearance_mouse ? molecule.data.drug_props.clearance_mouse : 'N/A'}
+          </td>
+          <td style={{ color: assaysRun.clearance_human ? (molecule.data.drug_props.clearance_human === "low (< 12)" ? 'green' : 'red') : 'black' }}>
+            {assaysRun.clearance_human ? molecule.data.drug_props.clearance_human : 'N/A'}
+          </td>
+          <td style={{ color: assaysRun.logd ? (molecule.data.drug_props.logd >= 1.00 ? 'green' : 'red') : 'black' }}>
+            {assaysRun.logd ? molecule.data.drug_props.logd : 'N/A'}
+          </td>
+          <td style={{ color: assaysRun.pampa ? (molecule.data.drug_props.pampa === "med2high" ? 'green' : 'red') : 'black' }}>
+            {assaysRun.pampa ? molecule.data.drug_props.pampa : 'N/A'}
+          </td>
+          <td style={{ color: assaysRun.pIC50 ? (molecule.data.drug_props.pic50 >= 6.0 ? 'green' : 'red') : 'black' }}>
+            {assaysRun.pIC50 ? molecule.data.drug_props.pic50 : 'N/A'}
+          </td>
         </tr>,
         isExpanded && (
           <tr key={`detail-${mol_id}`}>
@@ -35,6 +58,9 @@ const AssayDataTable = ({ saved_mols }) => {
         )
       ];
     });
+
+    // Combine the target profile row with the molecule rows
+    return [...targetProfileRow, ...moleculeRows];
   };
 
   return (
@@ -51,13 +77,12 @@ const AssayDataTable = ({ saved_mols }) => {
   );
 };
 
-
 function renderTableHeader() {
   return (
     <tr>
       <th>Compound ID</th>
-      <th>Mouse Clearance</th>
-      <th>Human Clearance</th>
+      <th>Mouse Clearance (mL/min)</th>
+      <th>Human Clearance (mL/min)</th>
       <th>LogD</th>
       <th>PAMPA</th>
       <th>pIC50</th>
--- src/components/analysis/analysis.js
+++ src/components/analysis/analysis.js
@@ -6,10 +6,14 @@ import ThePlot from "./the_plot.js";
 import AssayDataTable from "./AssayDataTable.js";
 import { connect } from "react-redux";
 import { initActions } from "../../actions";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+
 class Analysis extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showTable: true, // Initial state to show the table
+    };
   }
 
   // Retrieves Roche's target compound from the BE for reduced lag
@@ -18,30 +22,43 @@ class Analysis extends React.Component {
     this.props.fetchRoche();
   }
 
+  // Toggle function to show/hide the assay table
+  toggleTableVisibility = () => {
+    this.setState(prevState => ({
+      showTable: !prevState.showTable
+    }));
+  };
+
   render() {
     return (
       <div className="wrapper">
         {this.props.saved_or_not ? (
-        <div className="analysis">
-          <div className="final-molecule-bar">
-            <SelectorPanel />
-            <MoleculeList />
+          <div className="analysis">
+            <div className="final-molecule-bar">
+              <SelectorPanel />
+              <MoleculeList />
+            </div>
+            <div className="assay-data-table">
+              {this.state.showTable && <AssayDataTable />}
+            </div>
+            <div className="toggle-table-button">
+              <button onClick={this.toggleTableVisibility}>
+                {this.state.showTable ? 'Hide Assay Table' : 'Show Assay Table'}
+              </button>
+            </div>
+            <div className="nav-buttons">
+              <Link to="/assay">
+                <button>{'← Test'}</button>
+              </Link>
+            </div>
           </div>
-          <div className="assay-data-table">
-            <AssayDataTable />
-          </div>
-          <div className="nav-buttons">
-            <Link to="/assay">
-               <button>{'← Test'}</button>
+        ) : (
+          <div className='unsavedmol'>       
+            <Link to="/Loadingpage">
+              <button className="mk_pre_test_button">Go back to design your molecules first!</button>
             </Link>
           </div>
-        </div>) : (
-          <div className='unsavedmol'>       
-          <Link to="/Loadingpage">
-            <button className="mk_pre_test_button">Go back to design your molecules first!</button>
-          </Link></div>
-        ) 
-        }
+        )}
       </div>
     );
   }
